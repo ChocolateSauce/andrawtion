25fec67c40380dad0ef16baf7462cc2985dfeef5
ducksauce007 <ms1279@gmail.com>
2015-03-13 21:53:39 +0400

    Removed Nav Drawer
    Two Finger Long Press for Settings
    Two Finger Double Tap for Animation

--- gen/com/example/andrawtion/R.java
+++ gen/com/example/andrawtion/R.java
@@ -35,11 +35,8 @@ public final class R {
         public static final int pic_0360=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int content_frame=0x7f080005;
-        public static final int drawer_layout=0x7f080004;
+        public static final int action_settings=0x7f080004;
         public static final int imageView1=0x7f080000;
-        public static final int left_drawer=0x7f080006;
         public static final int level_ok=0x7f080003;
         public static final int level_seek=0x7f080002;
         public static final int level_txt=0x7f080001;
@@ -47,7 +44,6 @@ public final class R {
     public static final class layout {
         public static final int activity_start=0x7f030000;
         public static final int level_layout=0x7f030001;
-        public static final int navdraw_layout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/andrawtion/StartActivity.java
+++ src/com/example/andrawtion/StartActivity.java
@@ -2,33 +2,24 @@ package com.example.andrawtion;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.text.method.HideReturnsTransformationMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartActivity extends Activity {
 
-    int prog = 1;
+    long mSecondFingerDownTime = 0;
+	int prog = 1;
     int tprog = (prog+1)*10;
     int black = 8;
     int tblack = black*10;
@@ -36,10 +27,17 @@ public class StartActivity extends Activity {
     ImageView imview;
     float x1,x2;
     final float MIN_DISTANCE = 150;
-    
-    private String[] mPlanetTitles = {"hello", "hi", "ssup"};
-    private DrawerLayout mDrawerLayout;
-    private LinearLayout mDrawerList;
+    TwoFingerDoubleTapListener multiTouchListener = new TwoFingerDoubleTapListener() {
+        @Override
+        public void onTwoFingerDoubleTap() {
+            Toast.makeText(StartActivity.this, "Two Finger Double Tap", Toast.LENGTH_SHORT).show();
+            if(picAnimation.isRunning())
+  			  picAnimation.stop();
+  		  else
+  			  picAnimation.start();
+        }
+    };
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,15 +46,10 @@ public class StartActivity extends Activity {
         picAnimation = drawAnimation();
         imview = (ImageView) findViewById(R.id.imageView1);
         imview.setImageDrawable(picAnimation);
+        picAnimation.stop();
+        picAnimation.selectDrawable(0);
         
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.navdraw_layout);
-        mDrawerList = (LinearLayout) findViewById(R.id.left_drawer);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
     }
     
@@ -80,8 +73,35 @@ public class StartActivity extends Activity {
     }
     
     
-    private LinearLayout createSettings(LinearLayout lin) {
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("onOptionsItemSelected","yes");
+		if(picAnimation.isRunning())
+			  picAnimation.stop();
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			
+			buildSettings();
+
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void buildSettings() {
+		if(picAnimation.isRunning())
+			  picAnimation.stop();
+		final AlertDialog.Builder sup = new AlertDialog.Builder(this);  
+		sup.setTitle("Select Animation Level:");  
 		
+		LinearLayout lin = new LinearLayout(this);
 		lin.setOrientation(LinearLayout.VERTICAL);
 		
 		final TextView scaleText = new TextView(this);
@@ -132,97 +152,22 @@ public class StartActivity extends Activity {
 			@Override  
 			public void onStopTrackingTouch(SeekBar seekBar) {}  
 			});
-    	return lin;
-    	
-    }
-    
-    
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-    
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.d("onOptionsItemSelected","yes");
-		if(picAnimation.isRunning())
-			  picAnimation.stop();
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			
-			final AlertDialog.Builder sup = new AlertDialog.Builder(this);  
-			sup.setTitle("Select Animation Level:");  
-			
-			LinearLayout lin = new LinearLayout(this);
-			lin.setOrientation(LinearLayout.VERTICAL);
-			
-			final TextView scaleText = new TextView(this);
-			scaleText.setText("Animation Speed: x" + tprog);
-			final SeekBar scaleSeek = new SeekBar(this);
-			lin.addView(scaleText);
-			lin.addView(scaleSeek);
-			
-			scaleSeek.setMax(10); 
-			
-			scaleSeek.setProgress(prog);  
-			
-			
-			scaleSeek.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {    
-				@Override  
-				public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {  
-					prog = progress;
-					tprog = (prog+1)*10;
-					scaleText.setText("Animation Speed: x" + tprog);
-				}  
-				//methods to implement but not necessary to amend  
-				@Override  
-				public void onStartTrackingTouch(SeekBar seekBar) {}  
-				@Override  
-				public void onStopTrackingTouch(SeekBar seekBar) {}  
-				});
-			
-			final TextView blackText = new TextView(this);
-			blackText.setText("Blackout Duration: " + tblack + "ms");
-			final SeekBar blackSeek = new SeekBar(this);
-			lin.addView(blackText);
-			lin.addView(blackSeek);
-
-			blackSeek.setMax(10);  
-			blackSeek.setProgress(black);  
-			
-			
-			blackSeek.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {  
-				@Override  
-				public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {  
-					black = progress;
-					tblack = black*10;
-					blackText.setText("Blackout Duration: " + tblack + "ms");
-				}  
-				//methods to implement but not necessary to amend  
-				@Override  
-				public void onStartTrackingTouch(SeekBar seekBar) {}  
-				@Override  
-				public void onStopTrackingTouch(SeekBar seekBar) {}  
-				});
-			 
-			
-			sup.setPositiveButton("OK", new DialogInterface.OnClickListener() {			
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					//picAnimation.invalidateSelf();					
-					picAnimation = drawAnimation();
-					imview.setImageDrawable(picAnimation);
-					Toast.makeText(getApplicationContext(), "Saved",Toast.LENGTH_LONG).show();
-				}
-			});
-			
-			sup.setView(lin);
-			sup.show();
-
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		 
+		
+		sup.setPositiveButton("OK", new DialogInterface.OnClickListener() {			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//picAnimation.invalidateSelf();					
+				picAnimation = drawAnimation();
+				imview.setImageDrawable(picAnimation);
+		        picAnimation.stop();
+		        picAnimation.selectDrawable(0);
+				Toast.makeText(getApplicationContext(), "Saved",Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		sup.setView(lin);
+		sup.show();
 	}
 	
 	
@@ -235,30 +180,21 @@ public class StartActivity extends Activity {
 //		  else
 //			  picAnimation.start();
 //	    return true;
-//	  }
-    	switch(event.getAction())
-        {
-          case MotionEvent.ACTION_DOWN:
-              x1 = event.getX();  
-              if(picAnimation.isRunning())
-    			  picAnimation.stop();
-    		  else
-    			  picAnimation.start();
-              
-          break;         
-          case MotionEvent.ACTION_UP:
-              x2 = event.getX();
-              float deltaX = x2 - x1;
-              if (Math.abs(deltaX) > MIN_DISTANCE)
-              {
-                Toast.makeText(this, "left2right swipe", Toast.LENGTH_SHORT).show ();
-              }
-              else
-              {
-                  // consider as something else - a screen tap for example
-              }                          
-          break;   
-        } 
+//	  } 
+    	if(multiTouchListener.onTouchEvent(event))
+            return true;
+    	else{
+    	if(event.getActionMasked() == MotionEvent.ACTION_POINTER_DOWN && event.getActionIndex()==1){
+    	    mSecondFingerDownTime = System.currentTimeMillis();
+    	}
+
+    	if(event.getActionMasked() == MotionEvent.ACTION_POINTER_UP && event.getActionIndex()==1 ){
+    	    if ((System.currentTimeMillis()-mSecondFingerDownTime) >= 300) {
+    	        buildSettings();
+    	    	Toast.makeText(getApplicationContext(), "Two Finger Long Press",Toast.LENGTH_SHORT).show();
+    	    }
+    	}
+    	}
 	  return super.onTouchEvent(event);
     }
 
